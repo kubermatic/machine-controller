2941b538083ffc8b5a403c792a91fa12834624a3
Alvaro Aleman <alv2412@googlemail.com>
2018-02-06 17:04:13 +0100

    Deploy network

--- test/tools/integration/provision_master.sh
+++ test/tools/integration/provision_master.sh
@@ -7,10 +7,13 @@ export ADDR=$(cat terraform.tfstate |jq -r '.modules[0].resources["hcloud_server
 
 ssh_exec() { ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ADDR $@; }
 
+until ssh_exec exit; do sleep 1; done
+
 cat <<EOEXEC |ssh_exec
 set -ex
 
 if ! which docker; then
+  apt update
   apt install -y docker.io
   systemctl start docker
   systemctl status docker
@@ -23,12 +26,15 @@ if ! which kubelet; then
 EOF
   apt-get update
   apt-get install -y kubelet kubeadm kubectl
-  kubeadm init --apiserver-advertise-address=$ADDR
+  kubeadm init --apiserver-advertise-address=$ADDR --pod-network-cidr=10.244.0.0/16
 fi
 if ! ls \$HOME/.kube/config; then
   mkdir -p \$HOME/.kube
   cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
   kubectl taint nodes --all node-role.kubernetes.io/master-
 fi
-
+if ! ls kube-flannel.yml; then
+  curl -LO https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+  kubectl apply -f kube-flannel.yml
+fi
 EOEXEC
