5c141e317a1294e1c09354a7bd49850ea2bae7a4
Alvaro Aleman <alv2412@googlemail.com>
2018-02-06 16:48:45 +0100

    Deploy cluster

---
+++ test/tools/integration/provision_master.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export ADDR=$(cat terraform.tfstate |jq -r '.modules[0].resources["hcloud_server.machine-controller-test"].primary.attributes.ipv4_address')
+
+
+ssh_exec() { ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ADDR $@; }
+
+cat <<EOEXEC |ssh_exec
+set -ex
+
+if ! which docker; then
+  apt install -y docker.io
+  systemctl start docker
+  systemctl status docker
+fi
+if ! which kubelet; then
+  apt-get update && apt-get install -y apt-transport-https
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+  deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+  apt-get update
+  apt-get install -y kubelet kubeadm kubectl
+  kubeadm init --apiserver-advertise-address=$ADDR
+fi
+EOEXEC
