73bac6cb7c77ac0703d77bccc483cd9f2f897294
Alvaro Aleman <alv2412@googlemail.com>
2018-02-13 13:52:47 +0100

    Add func to retrieve values in referenced secrets

--- pkg/providerconfig/types.go
+++ pkg/providerconfig/types.go
@@ -3,9 +3,12 @@ package providerconfig
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
 )
 
 var (
@@ -38,14 +41,39 @@ type Config struct {
 	OperatingSystemSpec runtime.RawExtension `json:"operatingSystemSpec"`
 }
 
+// We can not use v1.SecretKeySelector because it is not cross namespace
+type GlobalSecretKeySelector struct {
+	v1.ObjectReference `json:",inline"`
+	Key                string `json:"key"`
+}
+
 type ConfigVarString struct {
-	Value     string              `json:"value,omitempty"`
-	ValueFrom *v1.ObjectReference `json:"valueFrom,omitempty"`
+	Value     string                  `json:"value,omitempty"`
+	ValueFrom GlobalSecretKeySelector `json:"valueFrom,omitempty"`
 }
 
 type ConfigVarBool struct {
-	Value     bool                `json:"value,omitempty"`
-	ValueFrom *v1.ObjectReference `json:"valueFrom,omitempty"`
+	Value     bool                    `json:"value,omitempty"`
+	ValueFrom GlobalSecretKeySelector `json:"valueFrom,omitempty"`
+}
+
+type SecretKeyGetter struct {
+	kubeClient kubernetes.Interface
+}
+
+func (secretKeyGetter *SecretKeyGetter) getConfigVarStringValue(kubeClient kubernetes.Interface, configVar ConfigVarString) (string, error) {
+	if configVar.Value != "" {
+		return configVar.Value, nil
+	}
+	secret, err := secretKeyGetter.kubeClient.CoreV1().Secrets(
+		configVar.ValueFrom.Namespace).Get(configVar.ValueFrom.Name, metav1.GetOptions{})
+	if err != nil {
+		return "", fmt.Errorf("error retrieving secret '%s' from namespace '%s': '%v'", configVar.ValueFrom.Name, configVar.ValueFrom.Namespace, err)
+	}
+	if val, ok := secret.Data[configVar.ValueFrom.Key]; ok {
+		return string(val), nil
+	}
+	return "", fmt.Errorf("secret '%s' in namespace '%s' has no key '%s'!", configVar.ValueFrom.Name, configVar.ValueFrom.Namespace, configVar.ValueFrom.Key)
 }
 
 func GetConfig(r runtime.RawExtension) (*Config, error) {
