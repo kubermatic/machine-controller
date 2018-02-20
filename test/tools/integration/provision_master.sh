733c70805166e473f8fe8858ea32b73a56926510
Alvaro Aleman <alv2412@googlemail.com>
2018-02-20 22:39:33 +0100

    Update samples to use stringData

--- test/tools/integration/provision_master.sh
+++ test/tools/integration/provision_master.sh
@@ -71,10 +71,10 @@ set -e
 echo "Testing create of a node via machine-controller...."
 ./verify \
   -input examples/machine-hetzner.yaml \
-  -parameters "<< HETZNER_TOKEN_BASE64_ENCODED >>=$(echo -n $HZ_TOKEN|base64 -w0)" \
+  -parameters "<< HETZNER_TOKEN_BASE64_ENCODED >>=$HZ_TOKEN" \
   -logtostderr true
 #./verify \
 #  -input examples/machine-digitalocean.yaml \
-#  -parameters "<< DIGITALOCEAN_TOKEN_BASE64_ENCODED >>=$(echo -n $DO_TOKEN|base64 -w0)" \
+#  -parameters "<< DIGITALOCEAN_TOKEN_BASE64_ENCODED >>=$DO_TOKEN" \
 #  -logtostderr true
 EOF
