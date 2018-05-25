7d6a02f39157cddd46babb9db6e8292e697bdf17
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-05-25 10:59:40 +0200

    add missing logout in vsphere client (#217)

--- test/e2e/provisioning/helper.go
+++ test/e2e/provisioning/helper.go
@@ -100,7 +100,6 @@ func runScenarios(st *testing.T, excludeSelector *scenarioSelector, testParams [
 			testScenario(it, testCase, cloudProvider, testParams, manifestPath)
 		})
 	}
-
 }
 
 func testScenario(t *testing.T, testCase scenario, cloudProvider string, testParams []string, manifestPath string) {
