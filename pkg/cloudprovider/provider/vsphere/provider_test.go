d3e8cfe5671f0960f4983f0a051cd8048b96038b
Alvaro Aleman <alv2412@googlemail.com>
2018-06-15 21:49:19 +0200

    Add some more tests for vsphere cloud provider

--- pkg/cloudprovider/provider/vsphere/provider_test.go
+++ pkg/cloudprovider/provider/vsphere/provider_test.go
@@ -9,30 +9,22 @@ import (
 )
 
 func TestGetCloudConfig(t *testing.T) {
-	p := provider{}
-	providerConfigRaw := []byte(`
+	tests := []struct {
+		providerConfig []byte
+		expected       string
+	}{
+		{providerConfig: []byte(`
 {
-  "cloudProvider": "vsphere",
   "cloudProviderSpec": {
-    "MemoryMB": 2048,
     "allowInsecure": true,
-    "cluster": "test-cluster",
-    "cpus": 2,
     "datacenter": "Datacenter",
     "datastore": "datastore1",
     "password": "password",
-    "templateVMName": "ubuntu-template",
     "username": "user",
     "vsphereURL": "https://your-vcenter:8443"
   }
-}`)
-	providerconfigRuntimeRawExtension := runtime.RawExtension{Raw: providerConfigRaw}
-	machineSpec := v1alpha1.MachineSpec{ProviderConfig: providerconfigRuntimeRawExtension}
-	cloudConfig, _, err := p.GetCloudConfig(machineSpec)
-	if err != nil {
-		t.Fatalf("Error rendering cloud-config: %v", err)
-	}
-	expected := `
+}`),
+			expected: `
 [Global]
 server = "your-vcenter"
 port = "8443"
@@ -41,8 +33,87 @@ password = "password"
 insecure-flag = "1" #set to 1 if the vCenter uses a self-signed cert
 datastore = "datastore1"
 working-dir = "/Datacenter/vm"
-datacenter = "Datacenter"`
-	if cloudConfig != expected {
-		t.Errorf("Cloud config was not as expected! Result: `%s`, Expected: `%s`", cloudConfig, expected)
+datacenter = "Datacenter"`,
+		},
+		{providerConfig: []byte(`
+{
+  "cloudProviderSpec": {
+    "allowInsecure": true,
+    "datacenter": "Datacenter",
+    "datastore": "datastore1",
+    "password": "password",
+    "username": "user",
+    "vsphereURL": "https://your-vcenter"
+  }
+}`),
+			expected: `
+[Global]
+server = "your-vcenter"
+port = "443"
+user = "user"
+password = "password"
+insecure-flag = "1" #set to 1 if the vCenter uses a self-signed cert
+datastore = "datastore1"
+working-dir = "/Datacenter/vm"
+datacenter = "Datacenter"`,
+		},
+		{providerConfig: []byte(`
+{
+  "cloudProviderSpec": {
+    "allowInsecure": false,
+    "datacenter": "Datacenter",
+    "datastore": "datastore1",
+    "password": "password",
+    "username": "user",
+    "vsphereURL": "https://your-vcenter"
+  }
+}`),
+			expected: `
+[Global]
+server = "your-vcenter"
+port = "443"
+user = "user"
+password = "password"
+insecure-flag = "0" #set to 1 if the vCenter uses a self-signed cert
+datastore = "datastore1"
+working-dir = "/Datacenter/vm"
+datacenter = "Datacenter"`,
+		},
+		{providerConfig: []byte(`
+{
+  "cloudProviderSpec": {
+    "allowInsecure": false,
+    "datacenter": "Datacenter",
+    "datastore": "datastore1",
+    "password": "password",
+    "username": "user",
+    "vsphereURL": "your-vcenter"
+  }
+}`),
+			expected: `
+[Global]
+server = "your-vcenter"
+port = "443"
+user = "user"
+password = "password"
+insecure-flag = "0" #set to 1 if the vCenter uses a self-signed cert
+datastore = "datastore1"
+working-dir = "/Datacenter/vm"
+datacenter = "Datacenter"`,
+		},
+	}
+
+	p := provider{}
+	for _, test := range tests {
+		providerconfigRuntimeRawExtension := runtime.RawExtension{Raw: test.providerConfig}
+		machineSpec := v1alpha1.MachineSpec{ProviderConfig: providerconfigRuntimeRawExtension}
+		cloudConfig, _, err := p.GetCloudConfig(machineSpec)
+		if err != nil {
+			t.Fatalf("Error rendering cloud-config: %v", err)
+		}
+		if cloudConfig != test.expected {
+			t.Errorf("Cloud config was not as expected! Result: `%s`, Expected: `%s`",
+				cloudConfig, test.expected)
+		}
 	}
 }
