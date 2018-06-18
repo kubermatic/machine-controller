3e7ba74e8b397c371bf304c2f6471bc05b2f3d43
mrIncompetent <henrik@henrik-schmidt.de>
2018-06-18 12:03:25 +0200

    log failed healthcheck

--- pkg/health/readiness.go
+++ pkg/health/readiness.go
@@ -4,11 +4,15 @@ import (
 	"github.com/heptiolabs/healthcheck"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"github.com/golang/glog"
 )
 
 func ApiserverReachable(client kubernetes.Interface) healthcheck.Check {
 	return func() error {
 		_, err := client.CoreV1().Nodes().List(metav1.ListOptions{})
+		if err != nil {
+			glog.V(0).Infof("unable to list nodes check: %v", err)
+		}
 		return err
 	}
 }
