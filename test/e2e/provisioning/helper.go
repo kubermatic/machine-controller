744f3113d41a1c5dd7f2485fbee150e6f147abe4
Alvaro Aleman <alv2412@googlemail.com>
2018-06-21 13:52:09 +0200

    Use cri-o 1.10 in e2e tests to be able to test k8s 1.10

--- test/e2e/provisioning/helper.go
+++ test/e2e/provisioning/helper.go
@@ -25,10 +25,10 @@ var scenarios = []scenario{
 	},
 
 	{
-		name:                    "scenario 3 Ubuntu CRI-O 1.9",
+		name:                    "scenario 3 Ubuntu CRI-O 1.10",
 		osName:                  "ubuntu",
 		containerRuntime:        "cri-o",
-		containerRuntimeVersion: "1.9",
+		containerRuntimeVersion: "1.10",
 	},
 
 	{
