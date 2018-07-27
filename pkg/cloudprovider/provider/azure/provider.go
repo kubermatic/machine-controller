32855418b102392756e5f8dce6191a9862e42305
Alvaro Aleman <alv2412@googlemail.com>
2018-07-27 17:26:30 +0200

    Always attempt to delete azure vms

--- pkg/cloudprovider/provider/azure/provider.go
+++ pkg/cloudprovider/provider/azure/provider.go
@@ -457,11 +457,9 @@ func (p *provider) Delete(machine *v1alpha1.Machine, update cloud.MachineUpdater
 	}
 
 	_, err = p.Get(machine)
-	if err != nil {
-		if err != cloudprovidererrors.ErrInstanceNotFound {
-			return err
-		}
-	} else {
+	// If a defunct VM got created, the `Get` call returns an error - But not because the request
+	// failed but because the VM has an invalid config hence always delete except on err == cloudprovidererrors.ErrInstanceNotFound
+	if err == nil || (err != nil && err != cloudprovidererrors.ErrInstanceNotFound) {
 		glog.Infof("deleting VM %q", machine.Name)
 		if err = deleteVMsByMachineUID(context.TODO(), config, machine.UID); err != nil {
 			return fmt.Errorf("failed to delete instance for  machine %q: %v", machine.Name, err)
