704aca582166de59b6658c1272350239bfc3b9f0
Alvaro Aleman <alv2412@googlemail.com>
2018-09-29 23:40:32 +0200

    Remove unused downstream machineset types

--- pkg/client/clientset/versioned/typed/machines/v1alpha1/machines_client.go
+++ pkg/client/clientset/versioned/typed/machines/v1alpha1/machines_client.go
@@ -28,7 +28,6 @@ import (
 type MachineV1alpha1Interface interface {
 	RESTClient() rest.Interface
 	MachinesGetter
-	MachineSetsGetter
 }
 
 // MachineV1alpha1Client is used to interact with features provided by the machine.k8s.io group.
@@ -38,10 +37,6 @@ type MachineV1alpha1Client struct {
 
 func (c *MachineV1alpha1Client) Machines() MachineInterface {
 	return newMachines(c)
-}
-
-func (c *MachineV1alpha1Client) MachineSets(namespace string) MachineSetInterface {
-	return newMachineSets(c, namespace)
 }
 
 // NewForConfig creates a new MachineV1alpha1Client for the given config.
