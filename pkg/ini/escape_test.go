6db6abd9901822d0788d5487e29ee53b9bf628f2
mrIncompetent <henrik@henrik-schmidt.de>
2018-10-23 07:18:29 +0200

    fix cyclic dependency

--- pkg/ini/escape_test.go
+++ pkg/ini/escape_test.go
@@ -1,13 +1,28 @@
 package ini
 
 import (
+	"bytes"
 	"testing"
+	"text/template"
 
-	"github.com/kubermatic/machine-controller/pkg/cloudprovider/provider/openstack"
-
+	"github.com/Masterminds/sprig"
 	"github.com/sethvargo/go-password/password"
 	"gopkg.in/gcfg.v1"
 )
+
+const (
+	testTpl = `[Global]
+Password = {{ .Global.Password | iniEscape }}
+`
+)
+
+type globalSection struct {
+	Password string
+}
+
+type testData struct {
+	Global globalSection
+}
 
 // TestINIEscape will ensure that we hopefully cover every case
 func TestINIEscape(t *testing.T) {
@@ -24,20 +39,28 @@ func TestINIEscape(t *testing.T) {
 
 		t.Logf("testing with pw: %s", pw)
 
-		before := &openstack.CloudConfig{
-			Global: openstack.GlobalOpts{
+		before := &testData{
+			Global: globalSection{
 				Password: pw,
 			},
 		}
 
-		s, err := openstack.CloudConfigToString(before)
+		funcMap := sprig.TxtFuncMap()
+		funcMap["iniEscape"] = INIEscape
+
+		tpl, err := template.New("test").Funcs(funcMap).Parse(testTpl)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to parse the cloud config template: %v", err)
 		}
 
-		after := &openstack.CloudConfig{}
-		if err := gcfg.ReadStringInto(after, s); err != nil {
-			t.Logf("\n%s", s)
+		buf := &bytes.Buffer{}
+		if err := tpl.Execute(buf, before); err != nil {
+			t.Fatalf("failed to execute cloud config template: %v", err)
+		}
+
+		after := &testData{}
+		if err := gcfg.ReadStringInto(after, buf.String()); err != nil {
+			t.Logf("\n%s", after)
 			t.Fatalf("failed to load string into config object: %v", err)
 		}
 
