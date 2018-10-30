1930965aa5b2d2d01bd9ec9cc5fe76babf94568c
Alvaro Aleman <alv2412@googlemail.com>
2018-10-30 22:15:01 +0100

    MachineDeployment: Validate machines on spec changes only

--- pkg/admission/machinedeployments.go
+++ pkg/admission/machinedeployments.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+
 	clusterv1alpha1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 )
 
@@ -21,8 +23,22 @@ func (ad *admissionData) mutateMachineDeployments(ar admissionv1beta1.AdmissionR
 		return nil, fmt.Errorf("validation failed: %v", errs)
 	}
 
-	if err := ad.defaultAndValidateMachineSpec(&machineDeployment.Spec.Template.Spec); err != nil {
-		return nil, err
+	// Do not validate the spec if it hasn't changed
+	machineSpecNeedsValidation := true
+	if ar.Request.Operation == admissionv1beta1.Update {
+		var oldMachineDeployment clusterv1alpha1.MachineDeployment
+		if err := json.Unmarshal(ar.Request.OldObject.Raw, &oldMachineDeployment); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal OldObject: %v", err)
+		}
+		if equal := apiequality.Semantic.DeepEqual(oldMachineDeployment.Spec.Template.Spec, machineDeployment.Spec.Template.Spec); equal {
+			machineSpecNeedsValidation = false
+		}
+	}
+
+	if machineSpecNeedsValidation {
+		if err := ad.defaultAndValidateMachineSpec(&machineDeployment.Spec.Template.Spec); err != nil {
+			return nil, err
+		}
 	}
 
 	return createAdmissionResponse(machineDeploymentOriginal, &machineDeployment)
