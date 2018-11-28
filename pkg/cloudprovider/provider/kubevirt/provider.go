5eec9920ab7148fe7bfba1b79a43f146fd3a12b4
Alvaro Aleman <alv2412@googlemail.com>
2018-11-28 17:06:28 +0100

    Initial kubevirt implementation

---
+++ pkg/cloudprovider/provider/kubevirt/provider.go
@@ -0,0 +1,281 @@
+package kubevirt
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/kubermatic/machine-controller/pkg/cloudprovider/cloud"
+	cloudprovidererrors "github.com/kubermatic/machine-controller/pkg/cloudprovider/errors"
+	"github.com/kubermatic/machine-controller/pkg/cloudprovider/instance"
+	"github.com/kubermatic/machine-controller/pkg/providerconfig"
+
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+
+	"k8s.io/apimachinery/pkg/types"
+	kubevirtv1 "kubevirt.io/kubevirt/pkg/api/v1"
+	"kubevirt.io/kubevirt/pkg/kubecli"
+
+	"sigs.k8s.io/cluster-api/pkg/apis/cluster/common"
+	"sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
+)
+
+type provider struct {
+	configVarResolver *providerconfig.ConfigVarResolver
+}
+
+// New returns a Kubevirt provider
+func New(configVarResolver *providerconfig.ConfigVarResolver) cloud.Provider {
+	return &provider{configVarResolver: configVarResolver}
+}
+
+type Config struct {
+	Config        rest.Config `json:"config"`
+	CPUs          int32       `json:"cpus"`
+	MemoryMiB     int64       `json:"memoryMIB"`
+	registryImage string      `json:"registryImage"`
+}
+
+type kubeVirtServer struct {
+	vmi kubevirtv1.VirtualMachineInstance
+}
+
+func (k *kubeVirtServer) Name() string {
+	return k.vmi.Name
+}
+
+func (k *kubeVirtServer) ID() string {
+	return string(k.vmi.UID)
+}
+
+func (k *kubeVirtServer) Addresses() []string {
+	var addresses []string
+	for _, kvInterface := range k.vmi.Status.Interfaces {
+		addresses = append(addresses, kvInterface.IP)
+	}
+	return addresses
+}
+
+func (k *kubeVirtServer) Status() instance.Status {
+	if k.vmi.Status.Phase == kubevirtv1.Running {
+		return instance.StatusRunning
+	}
+	return instance.StatusUnknown
+}
+
+var _ instance.Instance = &kubeVirtServer{}
+
+func (p *provider) getConfig(s v1alpha1.ProviderConfig) (*Config, *providerconfig.Config, error) {
+	if s.Value == nil {
+		return nil, nil, fmt.Errorf("machine.spec.providerconfig.value is nil")
+	}
+	pconfig := providerconfig.Config{}
+	err := json.Unmarshal(s.Value.Raw, &pconfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	//TODO: Use RawConfig to allow resolving via secretReg/ConfigMapRef
+	config := Config{}
+	if err = json.Unmarshal(pconfig.CloudProviderSpec.Raw, &config); err != nil {
+		return nil, nil, err
+	}
+
+	return &config, &pconfig, nil
+}
+
+func (p *provider) Get(machine *v1alpha1.Machine) (instance.Instance, error) {
+	c, _, err := p.getConfig(machine.Spec.ProviderConfig)
+	if err != nil {
+		return nil, cloudprovidererrors.TerminalError{
+			Reason:  common.InvalidConfigurationMachineError,
+			Message: fmt.Sprintf("Failed to parse MachineSpec, due to %v", err),
+		}
+	}
+	client, err := kubecli.GetKubevirtClientFromRESTConfig(&c.Config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get kubevirt client: %v", err)
+	}
+
+	//TODO: Should the namespace be configurable?
+	virtualMachineInstance, err := client.VirtualMachineInstance(metav1.NamespaceSystem).Get(string(machine.UID), &metav1.GetOptions{})
+	if err != nil {
+		if !kerrors.IsNotFound(err) {
+			return nil, fmt.Errorf("failed to get VirtualMachineInstance %s: %v", machine.UID, err)
+		}
+		return nil, cloudprovidererrors.ErrInstanceNotFound
+	}
+
+	if virtualMachineInstance.Status.Phase == kubevirtv1.Failed {
+		// The pod got deleted, delete the VMI and return ErrNotFound so the VMI
+		// will get recreated
+		if err := client.VirtualMachine(metav1.NamespaceSystem).Delete(string(machine.UID), &metav1.DeleteOptions{}); err != nil {
+			return nil, fmt.Errorf("failed to delete failed VMI %s: %v", machine.UID, err)
+		}
+		return nil, cloudprovidererrors.ErrInstanceNotFound
+	}
+
+	return &kubeVirtServer{vmi: *virtualMachineInstance}, nil
+}
+
+func (p *provider) MigrateUID(machine *v1alpha1.Machine, new types.UID) error {
+	return errors.New("not implemented")
+}
+
+func (p *provider) Validate(spec v1alpha1.MachineSpec) error {
+	c, _, err := p.getConfig(spec.ProviderConfig)
+	if err != nil {
+		return fmt.Errorf("failed to parse config: %v", err)
+	}
+	if c.CPUs < 1 {
+		return errors.New("CPUs must be 1 or greater")
+	}
+	if c.MemoryMiB < 1 {
+		return errors.New("MemoryMiB must be 1 or greater")
+	}
+	client, err := kubecli.GetKubevirtClientFromRESTConfig(&c.Config)
+	if err != nil {
+		return fmt.Errorf("failed to get kubevirt client: %v", err)
+	}
+	// Check if we can reach the API of the target cluster
+	_, err = client.VirtualMachineInstance(metav1.NamespaceSystem).Get("not-expected-to-exist", &metav1.GetOptions{})
+	if err != nil && !kerrors.IsNotFound(err) {
+		return fmt.Errorf("failed to request VirtualMachineInstances: %v", err)
+	}
+
+	return nil
+}
+
+func (p *provider) AddDefaults(spec v1alpha1.MachineSpec) (v1alpha1.MachineSpec, error) {
+	return spec, nil
+}
+
+func (p *provider) GetCloudConfig(spec v1alpha1.MachineSpec) (config string, name string, err error) {
+	return "", "", nil
+}
+
+func (p *provider) MachineMetricsLabels(machine *v1alpha1.Machine) (map[string]string, error) {
+	labels := make(map[string]string)
+
+	c, _, err := p.getConfig(machine.Spec.ProviderConfig)
+	if err == nil {
+		labels["cpus"] = strconv.Itoa(int(c.CPUs))
+		labels["memoryMIB"] = strconv.Itoa(int(c.MemoryMiB))
+		labels["registryImage"] = c.registryImage
+	}
+
+	return labels, err
+}
+
+func (p *provider) Create(machine *v1alpha1.Machine, _ *cloud.MachineCreateDeleteData, userdata string) (instance.Instance, error) {
+	c, _, err := p.getConfig(machine.Spec.ProviderConfig)
+	if err != nil {
+		return nil, cloudprovidererrors.TerminalError{
+			Reason:  common.InvalidConfigurationMachineError,
+			Message: fmt.Sprintf("Failed to parse MachineSpec, due to %v", err),
+		}
+	}
+
+	virtualMachineInstance := &kubevirtv1.VirtualMachineInstance{}
+	virtualMachineInstance.Name = string(machine.UID)
+	virtualMachineInstance.Namespace = metav1.NamespaceSystem
+	virtualMachineInstance.Spec.Domain.CPU = &kubevirtv1.CPU{Cores: uint32(c.CPUs)}
+	var disks []kubevirtv1.Disk
+	disks = append(disks, kubevirtv1.Disk{
+		Name:       "registryDisk",
+		VolumeName: "registryvolume",
+		DiskDevice: kubevirtv1.DiskDevice{Disk: &kubevirtv1.DiskTarget{Bus: "virtio"}},
+	})
+	disks = append(disks, kubevirtv1.Disk{
+		Name:       "cloudinitdisk",
+		VolumeName: "cloudinitvolume",
+		DiskDevice: kubevirtv1.DiskDevice{Disk: &kubevirtv1.DiskTarget{Bus: "virtio"}},
+	})
+	virtualMachineInstance.Spec.Domain.Devices.Disks = disks
+	//TODO: Remove the `MustParse`, we do not want the controller to crash
+	// on errors here
+	requestsAndLimits := corev1.ResourceList{
+		corev1.ResourceMemory: resource.MustParse(strconv.Itoa(int(c.MemoryMiB))),
+		corev1.ResourceCPU:    resource.MustParse(strconv.Itoa(int(c.CPUs))),
+	}
+	virtualMachineInstance.Spec.Domain.Resources.Requests = requestsAndLimits
+	virtualMachineInstance.Spec.Domain.Resources.Limits = requestsAndLimits
+	var volumes []kubevirtv1.Volume
+	volumes = append(volumes, kubevirtv1.Volume{
+		Name: "registryvolume",
+		VolumeSource: kubevirtv1.VolumeSource{
+			RegistryDisk: &kubevirtv1.RegistryDiskSource{Image: c.registryImage},
+		},
+	})
+
+	userdataSecretName := fmt.Sprintf("machine-controller-userdata-%s", machine.UID)
+	volumes = append(volumes, kubevirtv1.Volume{
+		Name: "cloudinitvolume",
+		VolumeSource: kubevirtv1.VolumeSource{
+			CloudInitNoCloud: &kubevirtv1.CloudInitNoCloudSource{
+				UserDataSecretRef: &corev1.LocalObjectReference{
+					Name: userdataSecretName,
+				},
+			},
+		},
+	})
+
+	client, err := kubecli.GetKubevirtClientFromRESTConfig(&c.Config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get kubevirt client: %v", err)
+	}
+
+	createdVMI, err := client.VirtualMachineInstance(virtualMachineInstance.Namespace).
+		Create(virtualMachineInstance)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create vmi: %v", err)
+	}
+
+	secretName := fmt.Sprintf("machine-controller-userdata-%s", createdVMI.UID)
+	secret := &corev1.Secret{}
+	secret.Name = secretName
+	secret.Namespace = createdVMI.Namespace
+	ownerRef := *metav1.NewControllerRef(createdVMI, kubevirtv1.VirtualMachineInstanceGroupVersionKind)
+	secret.OwnerReferences = append(secret.OwnerReferences, ownerRef)
+	if secret.Data == nil {
+		secret.Data = map[string][]byte{}
+	}
+	secret.Data["userdata"] = []byte(userdata)
+	_, err = client.CoreV1().Secrets(secret.Namespace).Create(secret)
+	// TODO: What happens when the pod for the vmi dies and we re-create the VMI?
+	// A secret with the given name may still exist
+	if err != nil {
+		return nil, fmt.Errorf("failed to create secret for userdata: %v", err)
+	}
+	return &kubeVirtServer{vmi: *createdVMI}, nil
+
+}
+
+func (p *provider) Cleanup(machine *v1alpha1.Machine, _ *cloud.MachineCreateDeleteData) (bool, error) {
+	c, _, err := p.getConfig(machine.Spec.ProviderConfig)
+	if err != nil {
+		return false, cloudprovidererrors.TerminalError{
+			Reason:  common.InvalidConfigurationMachineError,
+			Message: fmt.Sprintf("Failed to parse MachineSpec, due to %v", err),
+		}
+	}
+	client, err := kubecli.GetKubevirtClientFromRESTConfig(&c.Config)
+	if err != nil {
+		return false, fmt.Errorf("failed to get kubevirt client: %v", err)
+	}
+	_, err = client.VirtualMachineInstance(metav1.NamespaceSystem).Get(string(machine.UID), &metav1.GetOptions{})
+	if err != nil {
+		if !kerrors.IsNotFound(err) {
+			return false, fmt.Errorf("failed to get VirtualMachineInstance %s: %v", string(machine.UID), err)
+		}
+		// VMI is gone
+		return true, nil
+	}
+
+	return false, client.VirtualMachineInstance(metav1.NamespaceSystem).Delete(string(machine.UID), &metav1.DeleteOptions{})
+}
---
+++ vendor/kubevirt.io/kubevirt/pkg/kubecli/kubevirt.go
@@ -0,0 +1,124 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2018 Red Hat, Inc.
+ *
+ */
+
+package kubecli
+
+//go:generate mockgen -source $GOFILE -package=$GOPACKAGE -destination=generated_mock_$GOFILE
+
+/*
+ ATTENTION: Rerun code generators when interface signatures are modified.
+*/
+
+import (
+	"io"
+	"time"
+
+	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	cdiclient "kubevirt.io/containerized-data-importer/pkg/client/clientset/versioned"
+	"kubevirt.io/kubevirt/pkg/api/v1"
+)
+
+type KubevirtClient interface {
+	VirtualMachineInstance(namespace string) VirtualMachineInstanceInterface
+	VirtualMachineInstanceMigration(namespace string) VirtualMachineInstanceMigrationInterface
+	ReplicaSet(namespace string) ReplicaSetInterface
+	VirtualMachine(namespace string) VirtualMachineInterface
+	ServerVersion() *ServerVersion
+	RestClient() *rest.RESTClient
+	CdiClient() cdiclient.Interface
+	kubernetes.Interface
+}
+
+type kubevirt struct {
+	master     string
+	kubeconfig string
+	restClient *rest.RESTClient
+	config     *rest.Config
+	cdiClient  *cdiclient.Clientset
+	*kubernetes.Clientset
+}
+
+func (k kubevirt) CdiClient() cdiclient.Interface {
+	return k.cdiClient
+}
+
+func (k kubevirt) RestClient() *rest.RESTClient {
+	return k.restClient
+}
+
+type StreamOptions struct {
+	In  io.Reader
+	Out io.Writer
+}
+
+type StreamInterface interface {
+	Stream(options StreamOptions) error
+}
+
+type VirtualMachineInstanceInterface interface {
+	Get(name string, options *k8smetav1.GetOptions) (*v1.VirtualMachineInstance, error)
+	List(opts *k8smetav1.ListOptions) (*v1.VirtualMachineInstanceList, error)
+	Create(instance *v1.VirtualMachineInstance) (*v1.VirtualMachineInstance, error)
+	Update(*v1.VirtualMachineInstance) (*v1.VirtualMachineInstance, error)
+	Delete(name string, options *k8smetav1.DeleteOptions) error
+	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1.VirtualMachineInstance, err error)
+	SerialConsole(name string, timeout time.Duration) (StreamInterface, error)
+	VNC(name string) (StreamInterface, error)
+}
+
+type ReplicaSetInterface interface {
+	Get(name string, options k8smetav1.GetOptions) (*v1.VirtualMachineInstanceReplicaSet, error)
+	List(opts k8smetav1.ListOptions) (*v1.VirtualMachineInstanceReplicaSetList, error)
+	Create(*v1.VirtualMachineInstanceReplicaSet) (*v1.VirtualMachineInstanceReplicaSet, error)
+	Update(*v1.VirtualMachineInstanceReplicaSet) (*v1.VirtualMachineInstanceReplicaSet, error)
+	Delete(name string, options *k8smetav1.DeleteOptions) error
+}
+
+type VMIPresetInterface interface {
+	Get(name string, options k8smetav1.GetOptions) (*v1.VirtualMachineInstancePreset, error)
+	List(opts k8smetav1.ListOptions) (*v1.VirtualMachineInstancePresetList, error)
+	Create(*v1.VirtualMachineInstancePreset) (*v1.VirtualMachineInstancePreset, error)
+	Update(*v1.VirtualMachineInstancePreset) (*v1.VirtualMachineInstancePreset, error)
+	Delete(name string, options *k8smetav1.DeleteOptions) error
+	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1.VirtualMachineInstancePreset, err error)
+}
+
+// VirtualMachineInterface provides convenience methods to work with
+// virtual machines inside the cluster
+type VirtualMachineInterface interface {
+	Get(name string, options *k8smetav1.GetOptions) (*v1.VirtualMachine, error)
+	List(opts *k8smetav1.ListOptions) (*v1.VirtualMachineList, error)
+	Create(*v1.VirtualMachine) (*v1.VirtualMachine, error)
+	Update(*v1.VirtualMachine) (*v1.VirtualMachine, error)
+	Delete(name string, options *k8smetav1.DeleteOptions) error
+	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1.VirtualMachine, err error)
+}
+
+type VirtualMachineInstanceMigrationInterface interface {
+	Get(name string, options *k8smetav1.GetOptions) (*v1.VirtualMachineInstanceMigration, error)
+	List(opts *k8smetav1.ListOptions) (*v1.VirtualMachineInstanceMigrationList, error)
+	Create(*v1.VirtualMachineInstanceMigration) (*v1.VirtualMachineInstanceMigration, error)
+	Update(*v1.VirtualMachineInstanceMigration) (*v1.VirtualMachineInstanceMigration, error)
+	Delete(name string, options *k8smetav1.DeleteOptions) error
+	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1.VirtualMachineInstanceMigration, err error)
+}
