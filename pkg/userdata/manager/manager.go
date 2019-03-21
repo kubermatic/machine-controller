603418a698e1c20ca936978cbd1caecc2b15400c
Frank Mueller <frank@mweb.de>
2019-03-21 17:43:28 +0100

    Start with plugin registration process

--- pkg/userdata/manager/manager.go
+++ pkg/userdata/manager/manager.go
@@ -9,17 +9,55 @@
 package manager
 
 import (
+	"errors"
+	"io/ioutil"
+	"strings"
+	"sync"
+
 	"github.com/kubermatic/machine-controller/pkg/providerconfig"
 )
 
-// Manager is the responsible component for plugin discovery
-// and instantiation.
-type Manager struct {
-	providers map[providerconfig.OperatingSystem]*Plugin
+const (
+	// basePort is the first dynamic port for a found plugin.
+	// TODO Range has to be defined.
+	basePort = 60000
+
+	// pluginPrefix has to be the prefix of all plugin filenames.
+	pluginPrefix = "machine-controller-userdata-"
+)
+
+var (
+	// ErrPluginNotFound describes an invalid operating system for
+	// a user data plugin. Here directory has to be checked if
+	// correct ones are installed.
+	ErrPluginNotFound = errors.New("no user data plugin for the given operating system found")
+
+	// plugins contains all found and successfully started plugins.
+	plugins map[providerconfig.OperatingSystem]*plugin
+)
+
+// init creates the central manager instance.
+func init() {
+	plugins = make(map[providerconfig.OperatingSystem]*plugin)
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		// TODO Only logging or exit the machine controller?
+	}
+	for i, file := range files {
+		if strings.HasPrefix(file.Name(), pluginPrefix) {
+			p, err := newPlugin(file.Name(), basePort+i)
+			if err != nil {
+				// TODO Log and skip.
+			}
+			plugins[p.OperatingSystem()] = p
+		}
+	}
 }
 
-// New creates a new manager instance.
-func New() *Manager {
-	m := &Manager{}
-	return m
+// ForOS returns the plugin for the given operating system.
+func ForOS(os providerconfig.OperatingSystem) (p *Plugin, err error) {
+	if p, found = providers[os]; !found {
+		return nil, ErrPluginNotFound
+	}
+	return p, nil
 }
--- pkg/userdata/manager/plugin.go
+++ pkg/userdata/manager/plugin.go
@@ -4,14 +4,47 @@
 
 package manager
 
-// Plugin manages the communication to one plugin. It
-// instantiates it and hides all communication to
+import (
+	"net"
+
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	clusterv1alpha1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
+
+	"github.com/kubermatic/machine-controller/pkg/providerconfig"
+	"github.com/kubermatic/machine-controller/pkg/userdata/cloud"
+)
+
+// Plugin manages the communication to one plugin. It is instantiated
+// by the manager based on the directory scanning.
 type Plugin struct {
 	filename string
 	port     int
+	os       providerconfig.OperatingSystem
 }
 
-// newPlugin creates a new plugin manager.
+// newPlugin creates a new plugin manager. It starts the named
+// binary and connects to it via gRPC.
 func newPlugin(filename string, port int) (*Plugin, error) {
-	return nil, nil
+	p := &Plugin{
+		filename: filename,
+		port:     port,
+	}
+	return p, nil
+}
+
+// OperatingSystem returns the operating system this plugin is
+// responsible for.
+func (p *Plugin) OperatingSystem() providerconfig.OperatingSystem {
+	return p.os
+}
+
+// UserData retrieves the user data of the given resource via
+// plugin handling the communication.
+func (p *Plugin) UserData(
+	spec clusterv1alpha1.MachineSpec,
+	kubeconfig *clientcmdapi.Config,
+	ccProvider cloud.ConfigProvider,
+	clusterDNSIPs []net.IP,
+) (string, error) {
+	return "", nil
 }
