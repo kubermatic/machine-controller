5aec488d547d0b6edc8f17e9c018e5da93fe1c37
Frank Mueller <mail@themue.dev>
2019-04-16 16:28:34 +0200

    Move CPU and memory accounting settings to kubelet.service

--- pkg/userdata/coreos/provider.go
+++ pkg/userdata/coreos/provider.go
@@ -182,6 +182,8 @@ systemd:
         After=docker.service
         [Service]
         TimeoutStartSec=5min
+        CPUAccounting=true
+        MemoryAccounting=true
         Environment=KUBELET_IMAGE=docker://k8s.gcr.io/hyperkube-amd64:{{ .HyperkubeImageTag }}
         Environment="RKT_RUN_ARGS=--uuid-file-save=/var/cache/kubelet-pod.uuid \
           --insecure-options=image \
@@ -310,15 +312,6 @@ storage:
         inline: |
           [Service]
           Environment=DOCKER_OPTS=--storage-driver=overlay2
-
-    - path: /etc/systemd/system/kubelet.service.d/11-cgroups.conf
-      filesystem: root
-      mode: 0644
-      contents:
-        inline: |
-          [Service]
-          CPUAccounting=true
-          MemoryAccounting=true
 
     - path: /opt/bin/download.sh
       filesystem: root
