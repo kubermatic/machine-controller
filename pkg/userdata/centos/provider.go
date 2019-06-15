ac0401391ac3a432c942d5a76b8c60bffb575f00
Alvaro Aleman <alv2412@googlemail.com>
2019-06-15 18:13:26 +0200

    Fix conformance tests for CentOS
    With this change, docker on CentOS now logs to json-file rather than
    journald. This is required in order to make the conformance-tests pass on
    machine-controller-generated nodes. Without it, a changing subset of the
    tests constantly failed, their commonon denominator: They look for a
    string in the output of container logs and get an empty log and no error
    back.
    
    Signed-off-by: Alvaro Aleman <alv2412@googlemail.com>

--- pkg/userdata/centos/provider.go
+++ pkg/userdata/centos/provider.go
@@ -200,6 +200,8 @@ write_files:
     {{- if eq .CloudProviderName "vsphere" }}
     systemctl enable --now vmtoolsd.service
     {{ end -}}
+{{- /* Without this, the conformance tests fail with differing tests causing it, the common denominator: They look for some string in container logs and get an emtpy log */ -}}
+    sed -i 's/journald/json-file/g' /etc/sysconfig/docker
     systemctl enable --now docker
     systemctl enable --now kubelet
     systemctl enable --now --no-block kubelet-healthcheck.service
