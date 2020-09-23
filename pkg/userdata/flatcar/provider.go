79115879d83a57155d6c3603c5f0e3bda4a14e3c
Eduardo Oliveira de Carvalho <e.decarvalho@syseleven.de>
2020-09-23 14:06:57 +0200

    Add polkit rule for core user to reboot machine.
    Signed-off-by: Eduardo Oliveira de Carvalho <eduardooc.86@gmail.com>

--- pkg/userdata/flatcar/provider.go
+++ pkg/userdata/flatcar/provider.go
@@ -395,6 +395,22 @@ storage:
           PasswordAuthentication no
           ChallengeResponseAuthentication no
 
+    - path: "/etc/polkit-1/rules.d/60-noreboot_norestart.rule"
+      filesystem: root
+      mode: 0644
+      contents:
+        inline: |
+          polkit.addRule(function(action, subject) {
+              if (action.id == "org.freedesktop.login1.reboot" ||
+                  action.id == "org.freedesktop.login1.reboot-multiple-sessions") {
+                  if (subject.user == "core") {
+                      return polkit.Result.YES;
+                  } else {
+                      return polkit.Result.AUTH_ADMIN;
+                  }
+              }
+          });
+
     - path: /etc/docker/daemon.json
       filesystem: root
       mode: 0644
