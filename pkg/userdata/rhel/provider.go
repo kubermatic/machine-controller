c06f2f6badfb95ea7246e278332ce2df8536ca49
Iacopo Rozzo <iacopo@loodse.com>
2020-09-25 16:48:47 +0200

    [v1.14] Fix releasever to 7 for RHEL and CentOS in yum docker repository (#826) (#828)
    * Fix releasever to 7 for RHEL and CentOS in yum docker repository (#826)
    
    * Fix releasever to 7 for RHEL and CentOS in yum docker repository
    
    * Fix e2e tests
    
    * Disable CoreOS E2E tests on GCE and DigitalOcean (#822)
    
    * Disable CoreOS E2E tests on DigitalOcean
    
    * Disable CoreOS E2E tests on GCE
    
    * adjust the source url for the registry server (#794)
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    Co-authored-by: Marko Mudrinić <mudrinic.mare@gmail.com>
    Co-authored-by: Moath Qasim <moad.qassem@gmail.com>

--- pkg/userdata/rhel/provider.go
+++ pkg/userdata/rhel/provider.go
@@ -186,7 +186,12 @@ write_files:
     hostnamectl set-hostname {{ .MachineSpec.Name }}
     {{ end }}
 
-    dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    yum install -y yum-utils
+    yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+{{- /*	Due to DNF modules we have to do this on docker-ce repo
+		More info at: https://bugzilla.redhat.com/show_bug.cgi?id=1756473 */}}
+    sed -i 's/\$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
+    yum-config-manager --save --setopt=docker-ce-stable.module_hotfixes=true
 
     DOCKER_VERSION='18.09.1-3.el7'
     dnf install -y docker-ce-${DOCKER_VERSION} \
--- test/e2e/provisioning/all_e2e_test.go
+++ test/e2e/provisioning/all_e2e_test.go
@@ -159,7 +159,7 @@ func TestDigitalOceanProvisioningE2E(t *testing.T) {
 		t.Fatal("unable to run the test suite, DO_E2E_TESTS_TOKEN environement varialbe cannot be empty")
 	}
 
-	selector := Not(OsSelector("sles", "rhel", "flatcar"))
+	selector := Not(OsSelector("sles", "rhel", "flatcar", "coreos"))
 	// act
 	params := []string{fmt.Sprintf("<< DIGITALOCEAN_TOKEN >>=%s", doToken)}
 	runScenarios(t, selector, params, DOManifest, fmt.Sprintf("do-%s", *testRunIdentifier))
@@ -176,7 +176,7 @@ func TestAWSProvisioningE2E(t *testing.T) {
 	if len(awsKeyID) == 0 || len(awsSecret) == 0 {
 		t.Fatal("unable to run the test suite, AWS_E2E_TESTS_KEY_ID or AWS_E2E_TESTS_SECRET environment variables cannot be empty")
 	}
-	selector := Not(OsSelector("sles"))
+	selector := Not(OsSelector("coreos", "sles"))
 	// act
 	params := []string{fmt.Sprintf("<< AWS_ACCESS_KEY_ID >>=%s", awsKeyID),
 		fmt.Sprintf("<< AWS_SECRET_ACCESS_KEY >>=%s", awsSecret),
@@ -247,7 +247,7 @@ func TestAzureProvisioningE2E(t *testing.T) {
 		t.Fatal("unable to run the test suite, AZURE_TENANT_ID, AZURE_SUBSCRIPTION_ID, AZURE_CLIENT_ID and AZURE_CLIENT_SECRET environment variables cannot be empty")
 	}
 
-	selector := Not(OsSelector("sles"))
+	selector := Not(OsSelector("coreos", "sles"))
 	// act
 	params := []string{
 		fmt.Sprintf("<< AZURE_TENANT_ID >>=%s", azureTenantID),
@@ -271,7 +271,7 @@ func TestGCEProvisioningE2E(t *testing.T) {
 	}
 
 	// Act. GCE does not support CentOS.
-	selector := OsSelector("ubuntu", "coreos")
+	selector := OsSelector("ubuntu")
 	params := []string{
 		fmt.Sprintf("<< GOOGLE_SERVICE_ACCOUNT >>=%s", googleServiceAccount),
 	}
