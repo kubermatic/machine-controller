38141065d67a4761c423232f466936bf334834fa
Marko Mudrinić <mudrinic.mare@gmail.com>
2020-10-16 17:23:30 +0200

    Use Hyperkube Kubelet for 1.18 clusters (#837)
    * Use Hyperkube Kubelet for 1.18 clusters
    
    We already use Hyperkube Kubelet for clusters running Kubernetes
    1.17 and older. This PR extends this to 1.18, as there are no
    Poseidon Kubelet images for the latest 1.18 releases any longer.
    
    * Add 1.19.0 userdata test for Flatcar
    
    * Update test fixtures

--- pkg/userdata/flatcar/provider_test.go
+++ pkg/userdata/flatcar/provider_test.go
@@ -150,8 +150,8 @@ func TestUserDataGeneration(t *testing.T) {
 			osConfig: &Config{
 				DisableAutoUpdate: true,
 			},
-			hyperkubeImage: "for-kubernetes-less-then-1.18/hyperkubeImage",
-			kubeletImage:   "for-kubernetes-more-then-1.18/kubeletImage",
+			hyperkubeImage: "for-kubernetes-less-then-1.19/hyperkubeImage",
+			kubeletImage:   "for-kubernetes-more-then-1.19/kubeletImage",
 		},
 		{
 			name: "v1.18.0",
@@ -181,8 +181,39 @@ func TestUserDataGeneration(t *testing.T) {
 			osConfig: &Config{
 				DisableAutoUpdate: true,
 			},
-			hyperkubeImage: "for-kubernetes-less-then-1.18/hyperkubeImage",
-			kubeletImage:   "for-kubernetes-more-then-1.18/kubeletImage",
+			hyperkubeImage: "for-kubernetes-less-then-1.19/hyperkubeImage",
+			kubeletImage:   "for-kubernetes-more-then-1.19/kubeletImage",
+		},
+		{
+			name: "v1.19.0",
+			providerSpec: &providerconfigtypes.Config{
+				CloudProvider: "vsphere",
+				SSHPublicKeys: []string{"ssh-rsa AAABBB", "ssh-rsa CCCDDD"},
+				Network: &providerconfigtypes.NetworkConfig{
+					CIDR:    "192.168.81.4/24",
+					Gateway: "192.168.81.1",
+					DNS: providerconfigtypes.DNSConfig{
+						Servers: []string{"8.8.8.8"},
+					},
+				},
+			},
+			spec: clusterv1alpha1.MachineSpec{
+				ObjectMeta: metav1.ObjectMeta{Name: "node1"},
+				Versions: clusterv1alpha1.MachineVersionInfo{
+					Kubelet: "v1.19.0",
+				},
+			},
+			ccProvider: &fakeCloudConfigProvider{
+				name:   "vsphere",
+				config: "{vsphere-config:true}",
+				err:    nil,
+			},
+			DNSIPs: []net.IP{net.ParseIP("10.10.10.10")},
+			osConfig: &Config{
+				DisableAutoUpdate: true,
+			},
+			hyperkubeImage: "for-kubernetes-less-then-1.19/hyperkubeImage",
+			kubeletImage:   "for-kubernetes-more-then-1.19/kubeletImage",
 		},
 	}
 
