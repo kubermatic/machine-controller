ecb70e683750ae5fc7a7f18ae83ec09b9e3136a3
Christoph Mewes <christoph@loodse.com>
2020-12-18 19:20:49 +0100

    let admission handling run in its own goroutine

--- pkg/admission/admission.go
+++ pkg/admission/admission.go
@@ -106,31 +106,102 @@ func createAdmissionResponse(original, mutated runtime.Object) (*admissionv1beta
 
 type mutator func(context.Context, admissionv1beta1.AdmissionReview) (*admissionv1beta1.AdmissionResponse, error)
 
+// handleFuncFactory wraps a mutator as an HTTP HandlerFunc.
 func handleFuncFactory(mutate mutator) func(http.ResponseWriter, *http.Request) {
+	/*
+		Within the handler we create below, the actual mutation logic is
+		running in a separate goroutine. This is because sometimes some
+		cloud provider APIs are slow, very slow, and the kube-apiserver is
+		impatient and will wait at most 30 seconds (10 by default) for a
+		response.
+
+		To ensure faster responses, we use our CloudproviderCache wrapper,
+		which will cache the results based on the MachineSpec's JSON
+		representation. While this just generally speeds up responses, this
+		becomes *critical* for slow cloud providers. For those, it can happen
+		that no validation ever succeeds within the given timeout. To ensure
+		that these providers can be used at all, we must let the validation run
+		in the background, cache its result and inform the user upon a timeout
+		that the validation is still happening and they should try again later,
+		when we have cached the result.
+
+		We plan the timing below based on a 10s timeout, as this is much less
+		painful for users that would have to wait up to 30s for any kind of
+		feedback for their `kubectl apply` command.
+	*/
 	return func(w http.ResponseWriter, r *http.Request) {
-		// cancel any validation after at most 30 seconds, which is the maximum time
-		// the kube-apiserver will every wait for a webhook to answer
-		ctx, cancel := context.WithTimeout(r.Context(), 30*time.Second)
-		defer cancel()
+		// this context is used for the validation logic
+		validationCtx := context.Background()
 
-		// We must always return an AdmissionReview with an AdmissionResponse
-		// even on error, hence the admissionExecutor  func, this makes error handling much easier
-		admissionResponse, err := admissionExecutor(ctx, r, mutate)
-		if err != nil {
-			admissionResponse = &admissionv1beta1.AdmissionResponse{}
-			admissionResponse.Result = &metav1.Status{Message: err.Error()}
+		// kube-apiserver waits for 10s by default, so we give us 9s time
+		waitCtx, waitCancel := context.WithTimeout(validationCtx, 9*time.Second)
+		defer waitCancel()
+
+		// this is where we receive the validation result or error
+		responses := make(chan *admissionv1beta1.AdmissionResponse, 1)
+		errs := make(chan error, 1)
+
+		// start the validation
+		go func() {
+			response, err := admissionExecutor(validationCtx, r, mutate)
+			if err != nil {
+				errs <- err
+			} else {
+				responses <- response
+			}
+
+			close(responses)
+			close(errs)
+		}()
+
+		var response *admissionv1beta1.AdmissionResponse
+
+		// wait for either a result to appear or the timeout kicking in
+		select {
+		// happy path: validation completed
+		case response = <-responses:
+			// nop
+
+		// validation errored
+		case err := <-errs:
+			response = &admissionv1beta1.AdmissionResponse{
+				Result: &metav1.Status{
+					Message: err.Error(),
+				},
+			}
+
+		// give up and at least produce an informative error message for the user
+		case <-waitCtx.Done():
+			klog.Warning("failed to complete admission request within deadline; processing continues in the background, but the request has to be retried")
+
+			response = &admissionv1beta1.AdmissionResponse{
+				Result: &metav1.Status{
+					Message: "validation timed out, please try again later",
+					Code:    http.StatusGatewayTimeout,
+				},
+			}
+
+		// request has been cancelled
+		case <-r.Context().Done():
+			klog.Errorf("failed to complete admission request within deadline and validation request has been cancelled already")
 		}
 
-		admissionReview := admissionv1beta1.AdmissionReview{}
-		admissionReview.Response = admissionResponse
+		// if we have a response, send it out; otherwise the request has
+		// already been cancelled and writing a response is futile
+		if response != nil {
+			admissionReview := admissionv1beta1.AdmissionReview{
+				Response: response,
+			}
 
-		resp, err := json.Marshal(admissionReview)
-		if err != nil {
-			klog.Errorf("failed to marshal admissionResponse: %v", err)
-			return
-		}
-		if _, err := w.Write(resp); err != nil {
-			klog.Errorf("failed to write admissionResponse: %v", err)
+			resp, err := json.Marshal(admissionReview)
+			if err != nil {
+				klog.Errorf("failed to marshal admissionReview: %v", err)
+				return
+			}
+
+			if _, err := w.Write(resp); err != nil {
+				klog.Errorf("failed to write admissionReview: %v", err)
+			}
 		}
 	}
 }
