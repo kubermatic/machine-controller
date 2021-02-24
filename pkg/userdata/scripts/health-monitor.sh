88b46cccdaa3352d657c1b7a0d85a5ae029459da
Artiom Diomin <artiom@loodse.com>
2021-02-24 13:55:10 +0200

    Create workaround for kubelet stopped posting node status bug (#901)
    * Fix shell-check warnings in health-monitor.sh
    
    Signed-off-by: Artiom Diomin <kron82@gmail.com>
    
    * Check kubelet log for "use of closed network connection"
    
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- pkg/userdata/scripts/health-monitor.sh
+++ pkg/userdata/scripts/health-monitor.sh
@@ -30,7 +30,7 @@ set -o pipefail
 
 # We simply kill the process when there is a failure. Another systemd service will
 # automatically restart the process.
-function container_runtime_monitoring {
+function container_runtime_monitoring() {
   local -r max_attempts=5
   local attempt=1
   local -r crictl="${KUBE_HOME}/bin/crictl"
@@ -45,22 +45,22 @@ function container_runtime_monitoring {
   fi
   # Container runtime startup takes time. Make initial attempts before starting
   # killing the container runtime.
-  until timeout 60 ${healthcheck_command} > /dev/null; do
-    if (( attempt == max_attempts )); then
+  until timeout 60 "${healthcheck_command}" >/dev/null; do
+    if ((attempt == max_attempts)); then
       echo "Max attempt ${max_attempts} reached! Proceeding to monitor container runtime healthiness."
       break
     fi
     echo "$attempt initial attempt \"${healthcheck_command}\"! Trying again in $attempt seconds..."
-    sleep "$(( 2 ** attempt++ ))"
+    sleep "$((2 ** attempt++))"
   done
   while true; do
-    if ! timeout 60 ${healthcheck_command} > /dev/null; then
+    if ! timeout 60 "${healthcheck_command}" >/dev/null; then
       echo "Container runtime ${container_runtime_name} failed!"
       if [[ "$container_runtime_name" == "docker" ]]; then
-          # Dump stack of docker daemon for investigation.
-          # Log file name looks like goroutine-stacks-TIMESTAMP and will be saved to
-          # the exec root directory, which is /var/run/docker/ on Ubuntu and COS.
-          pkill -SIGUSR1 dockerd
+        # Dump stack of docker daemon for investigation.
+        # Log file name looks like goroutine-stacks-TIMESTAMP and will be saved to
+        # the exec root directory, which is /var/run/docker/ on Ubuntu and COS.
+        pkill -SIGUSR1 dockerd
       fi
       systemctl kill --kill-who=main "${container_runtime_name}"
       # Wait for a while, as we don't want to kill it again before it is really up.
@@ -71,16 +71,25 @@ function container_runtime_monitoring {
   done
 }
 
-function kubelet_monitoring {
+function kubelet_monitoring() {
   echo "Wait for 2 minutes for kubelet to be functional"
   # TODO(andyzheng0831): replace it with a more reliable method if possible.
   sleep 120
   local -r max_seconds=10
   local output=""
-  while [ 1 ]; do
-    if ! output=$(curl -m "${max_seconds}" -f -s -S http://127.0.0.1:10248/healthz 2>&1); then
+  while true; do
+    local failed=false
+
+    if journalctl -u kubelet -n 1 | grep -q "use of closed network connection"; then
+      failed=true
+      echo "Kubelet stopped posting node status. Restarting"
+    elif ! output=$(curl -m "${max_seconds}" -f -s -S http://127.0.0.1:10248/healthz 2>&1); then
+      failed=true
       # Print the response and/or errors.
-      echo $output
+      echo "$output"
+    fi
+
+    if [[ "$failed" == "true" ]]; then
       echo "Kubelet is unhealthy!"
       systemctl kill kubelet
       # Wait for a while, as we don't want to kill it again before it is really up.
@@ -90,7 +99,6 @@ function kubelet_monitoring {
     fi
   done
 }
-
 
 ############## Main Function ################
 if [[ "$#" -ne 1 ]]; then
@@ -108,5 +116,5 @@ if [[ "${component}" == "container-runtime" ]]; then
 elif [[ "${component}" == "kubelet" ]]; then
   kubelet_monitoring
 else
-  echo "Health monitoring for component "${component}" is not supported!"
+  echo "Health monitoring for component ${component} is not supported!"
 fi
