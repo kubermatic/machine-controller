3174ea20f23e0ef1cbebc8861cf48b22480b5a50
Artiom Diomin <artiom@loodse.com>
2021-06-16 20:01:20 +0300

    Force certificatesv1 to register (#1002)
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- cmd/machine-controller/main.go
+++ cmd/machine-controller/main.go
@@ -44,6 +44,7 @@ import (
 	"github.com/kubermatic/machine-controller/pkg/node"
 	"github.com/kubermatic/machine-controller/pkg/signals"
 
+	certificatesv1 "k8s.io/api/certificates/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
@@ -178,7 +179,10 @@ func main() {
 		klog.Fatalf("failed to add machinesv1alpha1 api to scheme: %v", err)
 	}
 	if err := apiextensionsv1.AddToScheme(scheme.Scheme); err != nil {
-		klog.Fatalf("failed to add apiextensionv1beta1 api to scheme: %v", err)
+		klog.Fatalf("failed to add apiextensionsv1 api to scheme: %v", err)
+	}
+	if err := certificatesv1.AddToScheme(scheme.Scheme); err != nil {
+		klog.Fatalf("failed to add certificatesv1 api to scheme: %v", err)
 	}
 	if err := clusterv1alpha1.AddToScheme(scheme.Scheme); err != nil {
 		klog.Fatalf("failed to add clusterv1alpha1 api to scheme: %v", err)
