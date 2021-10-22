59e4f9bd7fc2062df77a293ee594281ab3fa618f
Waleed Malik <ahmedwaleedmalik@gmail.com>
2021-10-22 14:58:05 +0500

    Update TLS Cipher Suites config for kubelet and restrict the allowed values

--- pkg/userdata/helper/kubelet.go
+++ pkg/userdata/helper/kubelet.go
@@ -172,6 +172,8 @@ func kubeletConfiguration(clusterDomain string, clusterDNS []net.IP, featureGate
 		KubeReserved:          map[string]string{"cpu": "200m", "memory": "200Mi", "ephemeral-storage": "1Gi"},
 		SystemReserved:        map[string]string{"cpu": "200m", "memory": "200Mi", "ephemeral-storage": "1Gi"},
 		VolumePluginDir:       "/var/lib/kubelet/volumeplugins",
+		TLSCipherSuites: []string{"TLS_AES_128_GCM_SHA256", "TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+			"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"},
 	}
 
 	buf, err := kyaml.Marshal(cfg)
