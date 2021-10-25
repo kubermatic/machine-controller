fd34d45c4013552b91593e3d053a85a999690c37
Waleed Malik <ahmedwaleedmalik@gmail.com>
2021-10-25 13:19:45 +0500

    Improve code styling

--- pkg/userdata/helper/kubelet.go
+++ pkg/userdata/helper/kubelet.go
@@ -86,8 +86,19 @@ const cpFlags = `--cloud-provider=%s \
 --cloud-config=/etc/kubernetes/cloud-config`
 
 // List of allowed TLS cipher suites for kubelet
-var kubeletTLSCipherSuites = []string{"TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384", "TLS_CHACHA20_POLY1305_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"}
+var kubeletTLSCipherSuites = []string{
+	// TLS 1.3 cipher suites
+	"TLS_AES_128_GCM_SHA256",
+	"TLS_AES_256_GCM_SHA384",
+	"TLS_CHACHA20_POLY1305_SHA256",
+	// TLS 1.0 - 1.2 cipher suites
+	"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+	"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
+	"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+	"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+	"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
+}
 
 // CloudProviderFlags returns --cloud-provider and --cloud-config flags
 func CloudProviderFlags(cpName string, external bool) (string, error) {
