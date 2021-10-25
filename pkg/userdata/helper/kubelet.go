6a0fa3b336296d9e9aad3ea7b69c71f176d3fe66
Waleed Malik <ahmedwaleedmalik@gmail.com>
2021-10-25 12:52:56 +0500

    Fix formatting error for kubelet.go

--- pkg/userdata/helper/kubelet.go
+++ pkg/userdata/helper/kubelet.go
@@ -87,7 +87,7 @@ const cpFlags = `--cloud-provider=%s \
 
 // List of allowed TLS cipher suites for kubelet
 var kubeletTLSCipherSuites = []string{"TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384", "TLS_CHACHA20_POLY1305_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"}
+	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305", "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"}
 
 // CloudProviderFlags returns --cloud-provider and --cloud-config flags
 func CloudProviderFlags(cpName string, external bool) (string, error) {
@@ -176,7 +176,7 @@ func kubeletConfiguration(clusterDomain string, clusterDNS []net.IP, featureGate
 		KubeReserved:          map[string]string{"cpu": "200m", "memory": "200Mi", "ephemeral-storage": "1Gi"},
 		SystemReserved:        map[string]string{"cpu": "200m", "memory": "200Mi", "ephemeral-storage": "1Gi"},
 		VolumePluginDir:       "/var/lib/kubelet/volumeplugins",
-		TLSCipherSuites: kubeletTLSCipherSuites,
+		TLSCipherSuites:       kubeletTLSCipherSuites,
 	}
 
 	buf, err := kyaml.Marshal(cfg)
