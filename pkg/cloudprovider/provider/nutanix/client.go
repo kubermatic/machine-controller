7c597e0f351b59b6862270b411e5fda91ee7965c
Marvin Beckers <marvin@kubermatic.com>
2021-12-13 15:18:57 +0100

    merge utils.go into client.go
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/cloudprovider/provider/nutanix/client.go
+++ pkg/cloudprovider/provider/nutanix/client.go
@@ -302,3 +302,50 @@ func getIPs(client *ClientSet, vmID string, interval time.Duration, timeout time
 
 	return addresses, nil
 }
+
+func waitForCompletion(client *ClientSet, taskID string, interval time.Duration, timeout time.Duration) error {
+	return wait.Poll(interval, timeout, func() (bool, error) {
+		task, err := client.Prism.V3.GetTask(taskID)
+		if err != nil {
+			return false, err
+		}
+
+		if task.Status == nil {
+			return false, nil
+		}
+
+		switch *task.Status {
+		case "INVALID_UUID", "FAILED":
+			return false, fmt.Errorf("bad status: %s", *task.Status)
+		case "QUEUED", "RUNNING":
+			return false, nil
+		case "SUCCEEDED":
+			return true, nil
+		default:
+			return false, fmt.Errorf("unknown status: %s", *task.Status)
+		}
+
+	})
+}
+
+func waitForPowerState(client *ClientSet, vmID string, interval time.Duration, timeout time.Duration) error {
+	return wait.Poll(interval, timeout, func() (bool, error) {
+		vm, err := client.Prism.V3.GetVM(vmID)
+		if err != nil {
+			return false, err
+		}
+
+		if vm.Status == nil || vm.Status.Resources == nil || vm.Status.Resources.PowerState == nil {
+			return false, nil
+		}
+
+		switch *vm.Status.Resources.PowerState {
+		case "ON":
+			return true, nil
+		case "OFF":
+			return false, nil
+		default:
+			return false, fmt.Errorf("unexpected power state: %s", *vm.Status.Resources.PowerState)
+		}
+	})
+}
