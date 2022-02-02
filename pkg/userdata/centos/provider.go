88b7a9f080ecea2a9a7f0ea2d1f665e7e5a5ded1
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-02-02 12:05:01 +0100

    Change CentOS baseurl to vault.centos.org
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/userdata/centos/provider.go
+++ pkg/userdata/centos/provider.go
@@ -206,6 +206,13 @@ write_files:
     hostnamectl set-hostname {{ .MachineSpec.Name }}
     {{ end }}
 
+{{- /* CentOS 8 has reached EOL and all packages were moved to vault.centos.org -- https://www.centos.org/centos-linux-eol/ */}}
+    source /etc/os-release
+    if [ "$ID" == "centos" ] && [ "$VERSION_ID" == "8" ]; then
+      sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+      sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+    fi
+
     yum install -y \
       device-mapper-persistent-data \
       lvm2 \
