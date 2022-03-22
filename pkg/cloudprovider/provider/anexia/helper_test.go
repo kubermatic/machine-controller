6e26a644fa10890a9166305480eb9e5b66eed2f1
Mara Sophie Grosch <mgrosch@anexia-it.com>
2022-03-22 11:52:57 +0100

    Add missing boilerplate to files
    Signed-off-by: Mara Sophie Grosch <mgrosch@anexia-it.com>

--- pkg/cloudprovider/provider/anexia/helper_test.go
+++ pkg/cloudprovider/provider/anexia/helper_test.go
@@ -1,15 +1,32 @@
+/*
+Copyright 2022 The Machine Controller Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package anexia
 
 import (
 	"encoding/json"
+	"net/http"
+	"testing"
+
 	"github.com/anexia-it/go-anxcloud/pkg/vsphere/search"
 	"github.com/gophercloud/gophercloud/testhelper"
 	"github.com/kubermatic/machine-controller/pkg/apis/cluster/v1alpha1"
 	anxtypes "github.com/kubermatic/machine-controller/pkg/cloudprovider/provider/anexia/types"
 	"github.com/kubermatic/machine-controller/pkg/providerconfig/types"
 	"k8s.io/apimachinery/pkg/runtime"
-	"net/http"
-	"testing"
 )
 
 type ConfigTestCase struct {
