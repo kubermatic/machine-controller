f09820656f8070f096d87fbe645197f097d8211e
Moath Qasim <moad.qassem@gmail.com>
2022-04-08 21:14:20 +0200

    support rocky linux for OS (#1248)
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- test/e2e/provisioning/all_e2e_test.go
+++ test/e2e/provisioning/all_e2e_test.go
@@ -321,7 +321,7 @@ func TestOpenstackProvisioningE2E(t *testing.T) {
 		fmt.Sprintf("<< NETWORK_NAME >>=%s", osNetwork),
 	}
 
-	selector := Not(OsSelector("sles", "rhel", "amzn2", "rockylinux"))
+	selector := Not(OsSelector("sles", "rhel", "amzn2"))
 	runScenarios(t, selector, params, OSManifest, fmt.Sprintf("os-%s", *testRunIdentifier))
 }
 
