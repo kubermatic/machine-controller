c66cc2627dbb147bb68a77e97884446c7ce6562d
Waleed Malik <ahmedwaleedmalik@gmail.com>
2022-07-29 13:43:41 +0500

    Add label on machine to differentiate between the tool used for managing machine configurations
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/admission/machines.go
+++ pkg/admission/machines.go
@@ -27,6 +27,7 @@ import (
 	"github.com/kubermatic/machine-controller/pkg/apis/cluster/common"
 	clusterv1alpha1 "github.com/kubermatic/machine-controller/pkg/apis/cluster/v1alpha1"
 	"github.com/kubermatic/machine-controller/pkg/cloudprovider"
+	controllerutil "github.com/kubermatic/machine-controller/pkg/controller/util"
 	"github.com/kubermatic/machine-controller/pkg/providerconfig"
 	providerconfigtypes "github.com/kubermatic/machine-controller/pkg/providerconfig/types"
 
@@ -95,6 +96,14 @@ func (ad *admissionData) mutateMachines(ctx context.Context, ar admissionv1.Admi
 			return nil, err
 		}
 		common.SetOSLabel(&machine.Spec, string(providerConfig.OperatingSystem))
+	}
+
+	// Set LegacyMachineControllerUserDataLabel to false if OSM was used for managing the machine configuration.
+	if ad.useOSM {
+		if machine.Labels == nil {
+			machine.Labels = make(map[string]string)
+		}
+		machine.Labels[controllerutil.LegacyMachineControllerUserDataLabel] = "false"
 	}
 
 	return createAdmissionResponse(machineOriginal, &machine)
