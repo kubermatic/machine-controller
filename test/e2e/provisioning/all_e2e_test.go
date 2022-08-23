9cb3a6db4fcd1324881ca33f65c759adfa20d582
Waleed Malik <ahmedwaleedmalik@gmail.com>
2022-08-23 17:44:48 +0500

    Fix E2E tests for vSphere and OpenStack (#1412)
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>
    
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- test/e2e/provisioning/all_e2e_test.go
+++ test/e2e/provisioning/all_e2e_test.go
@@ -871,7 +871,7 @@ func getVSphereTestParams(t *testing.T) []string {
 func TestVsphereProvisioningE2E(t *testing.T) {
 	t.Parallel()
 
-	selector := Not(OsSelector("sles", "amzn2"))
+	selector := Not(OsSelector("sles", "amzn2", "rockylinux"))
 	params := getVSphereTestParams(t)
 
 	runScenarios(t, selector, params, VSPhereManifest, fmt.Sprintf("vs-%s", *testRunIdentifier))
