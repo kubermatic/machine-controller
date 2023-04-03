767cc56a8108aabc7f492a31967151b193c422e6
Waleed Malik <ahmedwaleedmalik@gmail.com>
2023-04-03 18:28:14 +0500

    Refactored code
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/cloudprovider/provider/vmwareclouddirector/provider.go
+++ pkg/cloudprovider/provider/vmwareclouddirector/provider.go
@@ -332,37 +332,37 @@ func (p *provider) getConfig(provSpec clusterv1alpha1.ProviderSpec) (*Config, *p
 
 	c.APIToken, err = p.configVarResolver.GetConfigVarStringValueOrEnv(rawConfig.APIToken, "VCD_API_TOKEN")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"apiToken\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "apiToken" field, error = %w`, err)
 	}
 
 	c.Username, err = p.configVarResolver.GetConfigVarStringValueOrEnv(rawConfig.Username, "VCD_USER")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"username\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "username" field, error = %w`, err)
 	}
 
 	c.Password, err = p.configVarResolver.GetConfigVarStringValueOrEnv(rawConfig.Password, "VCD_PASSWORD")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"password\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "password" field, error = %w`, err)
 	}
 
 	c.Organization, err = p.configVarResolver.GetConfigVarStringValueOrEnv(rawConfig.Organization, "VCD_ORG")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"organization\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "organization" field, error = %w`, err)
 	}
 
 	c.URL, err = p.configVarResolver.GetConfigVarStringValueOrEnv(rawConfig.URL, "VCD_URL")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"url\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "url" field, error = %w`, err)
 	}
 
 	c.VDC, err = p.configVarResolver.GetConfigVarStringValueOrEnv(rawConfig.VDC, "VCD_VDC")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"vdc\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "vdc" field, error = %w`, err)
 	}
 
 	c.AllowInsecure, err = p.configVarResolver.GetConfigVarBoolValueOrEnv(rawConfig.AllowInsecure, "VCD_ALLOW_UNVERIFIED_SSL")
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get the value of \"allowInsecure\" field, error = %w", err)
+		return nil, nil, nil, fmt.Errorf(`failed to get the value of "allowInsecure" field, error = %w`, err)
 	}
 
 	c.VApp, err = p.configVarResolver.GetConfigVarStringValue(rawConfig.VApp)
@@ -388,30 +388,30 @@ func (p *provider) getConfig(provSpec clusterv1alpha1.ProviderSpec) (*Config, *p
 	c.IPAllocationMode = rawConfig.IPAllocationMode
 
 	if rawConfig.DiskSizeGB != nil && *rawConfig.DiskSizeGB < 0 {
-		return nil, nil, nil, fmt.Errorf("value for \"diskSizeGB\" should either be nil or greater than or equal to 0")
+		return nil, nil, nil, fmt.Errorf(`value for "diskSizeGB" should either be nil or greater than or equal to 0`)
 	}
 	c.DiskSizeGB = rawConfig.DiskSizeGB
 
 	if rawConfig.DiskIOPS != nil && *rawConfig.DiskIOPS < 0 {
-		return nil, nil, nil, fmt.Errorf("value for \"diskIOPS\" should either be nil or greater than or equal to 0")
+		return nil, nil, nil, fmt.Errorf(`value for "diskIOPS" should either be nil or greater than or equal to 0`)
 	}
 	c.DiskIOPS = rawConfig.DiskIOPS
 
 	if rawConfig.CPUs <= 0 {
-		return nil, nil, nil, fmt.Errorf("value for \"cpus\" should be greater than 0")
+		return nil, nil, nil, fmt.Errorf(`value for "cpus" should be greater than 0`)
 	}
 	c.CPUs = rawConfig.CPUs
 
 	if rawConfig.CPUCores <= 0 {
-		return nil, nil, nil, fmt.Errorf("value for \"cpuCores\" should be greater than 0")
+		return nil, nil, nil, fmt.Errorf(`value for "cpuCores" should be greater than 0`)
 	}
 	c.CPUCores = rawConfig.CPUCores
 
 	if rawConfig.MemoryMB <= 4 {
-		return nil, nil, nil, fmt.Errorf("value for \"memoryMB\" should be greater than 0")
+		return nil, nil, nil, fmt.Errorf(`value for "memoryMB" should be greater than 0`)
 	}
 	if rawConfig.MemoryMB%4 != 0 {
-		return nil, nil, nil, fmt.Errorf("value for \"memoryMB\" should be a multiple of 4")
+		return nil, nil, nil, fmt.Errorf(`value for "memoryMB" should be a multiple of 4`)
 	}
 	c.MemoryMB = rawConfig.MemoryMB
 
@@ -482,7 +482,7 @@ func (p *provider) Validate(_ context.Context, spec clusterv1alpha1.MachineSpec)
 	}
 
 	if c.APIToken != "" && (c.Password != "" || c.Username != "") {
-		return fmt.Errorf("either \"apiToken\" or \"username\" and \"password\" must be specified")
+		return fmt.Errorf(`either "apiToken" or "username" and "password" must be specified`)
 	}
 
 	client, err := NewClient(c.Username, c.Password, c.APIToken, c.Organization, c.URL, c.VDC, c.AllowInsecure)
