131c9a401ef1ca161d6facbf11dc45f538f97fcb
Christoph Mewes <christoph@kubermatic.com>
2024-05-22 20:40:58 +0200

    use Ubuntu 24.04 in vSphere e2e tests

--- test/e2e/provisioning/helper.go
+++ test/e2e/provisioning/helper.go
@@ -67,7 +67,7 @@ var (
 		string(providerconfigtypes.OperatingSystemFlatcar):    "kkp-flatcar-3139.2.0",
 		string(providerconfigtypes.OperatingSystemRHEL):       "kkp-rhel-8.6",
 		string(providerconfigtypes.OperatingSystemRockyLinux): "kkp-rockylinux-8",
-		string(providerconfigtypes.OperatingSystemUbuntu):     "kkp-ubuntu-22.04",
+		string(providerconfigtypes.OperatingSystemUbuntu):     "kkp-ubuntu-24.04",
 	}
 
 	kubevirtImages = map[string]string{
