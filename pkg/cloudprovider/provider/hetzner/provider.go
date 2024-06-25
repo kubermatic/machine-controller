9db708c6da83e8b49800ca6cf41a18adc3dac696
Pascal Sthamer <10992664+P4sca1@users.noreply.github.com>
2024-06-25 10:13:19 +0200

    Hetzner: Skip operating system validation if custom image is provided (#1818)
    This should make it possible to use operating systems on Hetzner Cloud, that are not natively supported, by using custom images (snapshots).
    
    A side-effect of this commit is, that images returned by `getNameForOs()` are now also validated against the Hetzner API.
    
    For reference: #1817
    
    Requirements to use custom operating systems:
    1. Create a snapshot of the operating system you want to use on Hetzner Cloud, e.g. using https://github.com/apricote/hcloud-upload-image/
    2. Add a new `OperatingSystemProfile`
    3. Set the image in `MachineDeployment` to the name of the snapshot created in step 1
    
    Signed-off-by: Pascal Sthamer <10992664+P4sca1@users.noreply.github.com>

--- pkg/cloudprovider/provider/hetzner/provider.go
+++ pkg/cloudprovider/provider/hetzner/provider.go
@@ -200,11 +200,6 @@ func (p *provider) Validate(ctx context.Context, _ *zap.SugaredLogger, spec clus
 		return errors.New("token is missing")
 	}
 
-	_, err = getNameForOS(pc.OperatingSystem)
-	if err != nil {
-		return fmt.Errorf("invalid/not supported operating system specified %q: %w", pc.OperatingSystem, err)
-	}
-
 	client := getClient(c.Token)
 
 	if c.Location != "" && c.Datacenter != "" {
@@ -223,11 +218,17 @@ func (p *provider) Validate(ctx context.Context, _ *zap.SugaredLogger, spec clus
 		}
 	}
 
-	if c.Image != "" {
-		//nolint:staticcheck // We do not have the architecture available here.
-		if _, _, err = client.Image.Get(ctx, c.Image); err != nil {
-			return fmt.Errorf("failed to get image: %w", err)
+	image := c.Image
+	if image == "" {
+		image, err = getNameForOS(pc.OperatingSystem)
+		if err != nil {
+			return fmt.Errorf("invalid/not supported operating system specified %q: %w", pc.OperatingSystem, err)
 		}
+	}
+
+	//nolint:staticcheck // We do not have the architecture available here.
+	if _, _, err = client.Image.Get(ctx, image); err != nil {
+		return fmt.Errorf("failed to get image: %w", err)
 	}
 
 	for _, network := range c.Networks {
