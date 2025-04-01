7b6ae89532d1ef525806fdba37d80801b607d0d0
soer3n <43064202+soer3n@users.noreply.github.com>
2025-04-01 15:04:05 +0200

    fix kubevirt cpu check + update sdk version in go.mod (#1912)
    * fix kubevirt cpu check + update sdk version in go.mod
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    * revert change regarding import of sdk submodule
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    * kubevirt resources and vcpus should only be parsed wihtout specified instance type
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    ---------
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>

--- pkg/cloudprovider/provider/kubevirt/provider.go
+++ pkg/cloudprovider/provider/kubevirt/provider.go
@@ -285,9 +285,11 @@ func (p *provider) getConfig(provSpec clusterv1alpha1.ProviderSpec) (*Config, *p
 		return nil, nil, fmt.Errorf(`failed to get value of "memory" field: %w`, err)
 	}
 
-	config.Resources, config.VCPUs, err = parseResources(cpus, memory, rawConfig.VirtualMachine.Template.VCPUs)
-	if err != nil {
-		return nil, nil, fmt.Errorf(`failed to configure resource requests and limits and vcpus: %w`, err)
+	if rawConfig.VirtualMachine.Instancetype == nil {
+		config.Resources, config.VCPUs, err = parseResources(cpus, memory, rawConfig.VirtualMachine.Template.VCPUs)
+		if err != nil {
+			return nil, nil, fmt.Errorf(`failed to configure resource requests and limits and vcpus: %w`, err)
+		}
 	}
 
 	config.Namespace = getNamespace()
@@ -629,11 +631,11 @@ func (p *provider) Validate(ctx context.Context, _ *zap.SugaredLogger, spec clus
 			return fmt.Errorf("no resource requests set for the virtual machine")
 		}
 
-		if c.VCPUs == nil && c.Resources.Cpu() == nil {
+		if c.VCPUs == nil && c.Resources.Cpu().IsZero() {
 			return fmt.Errorf("no CPUs configured. Either vCPUs or CPUs have to be set.")
 		}
 
-		if c.VCPUs != nil && c.Resources.Cpu() != nil {
+		if c.VCPUs != nil && !c.Resources.Cpu().IsZero() {
 			return fmt.Errorf("vCPUs and CPUs cannot be configured at the same time.")
 		}
 	}
